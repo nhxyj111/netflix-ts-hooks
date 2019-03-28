a6a587579ac7130e8ca3f3d0234a52d0a51e4342
xuyejian <nhxyj111@gmail.com>
2019-03-28 15:57:17 +0800

    api & mockdata (without Notification)

---
+++ src/api/util.ts
@@ -0,0 +1,22 @@
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Image } from "react-native";
+
+export const cacheFonts = (
+  fonts: Array<string | { [name: string]: string | number | Asset }>
+) => {
+  const mappedFonts = fonts.map(font => Font.loadAsync(font));
+  return mappedFonts;
+};
+
+export const cacheImages = (images: { [name: string]: any }) => {
+  const imagesArray = Object.values(images);
+
+  return imagesArray.map(image => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    }
+
+    return Asset.fromModule(image).downloadAsync();
+  });
+};
